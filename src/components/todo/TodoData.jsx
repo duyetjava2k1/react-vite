36ff543cd52d3bca00a5819f2b1da442ba9927b0
duyetjava2k1 <giangvanduyet_t64@hus.edu.vn>
2024-11-04 08:12:04 +0700

    v33

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,12 @@
+const TodoData = () => {
+    return(
+        <>
+            <div className="todo-data">
+                <div>Watching Youtobe</div>
+                <div>Learning React</div>
+            </div>
+        </>
+    )
+}
+
+export default TodoData;
